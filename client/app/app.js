79a4bae00ee0522e106443408477ff7fe7624920
Roberto Rojas <robertojrojas@gmail.com>
2015-04-29 19:18:55 -0400

    added angularui route.

--- client/app/app.js
+++ client/app/app.js
@@ -2,37 +2,48 @@
     'use strict';
     var app = angular.module('selfRegistrationLoopBack', [
         // Angular modules
-        'ngRoute'
 
         // 3rd Party Modules
+        'ui.router'
 
     ]);
 
-    app.config(['$routeProvider', configRoutes]);
+    app.config(['$stateProvider', '$urlRouterProvider', configRoutes]);
 
-    function configRoutes($routeProvider) {
-        $routeProvider
-            .when('/', {
+    function configRoutes($stateProvider, $urlRouterProvider) {
+      $stateProvider
+            .state('home', {
+                url: '/',
                 templateUrl: 'app/home/home.html',
                 controller: 'HomeCtrl',
                 controllerAs: 'vm'
             })
-            .when('/preferences', {
+            .state('preferences', {
+              url: '/preferences',
               templateUrl: 'app/preferences/preferences.html',
               controller: 'PreferencesCtrl',
               controllerAs: 'preferencesVm'
             })
-            .when('/signup', {
+            .state('signup', {
+              url: '/signup',
               templateUrl: 'app/signup/signup.html',
-              controller: 'SignupCtrl',
+              controller: 'SignUpCtrl',
               controllerAs: 'signupVm'
-             });
-
-
-        $routeProvider.otherwise('/');
+             })
+            .state('forbidden', {
+              url: '/forbidden',
+              templateUrl: 'views/forbidden.html'
+            });
+      $urlRouterProvider.otherwise('home');
     }
 
-    app.run(['$route', function ($route) {
-        // Include $route to kick start the router.
+    app.run(['$rootScope', '$state', function($rootScope, $state) {
+      $rootScope.$on('$stateChangeStart', function(event, next) {
+        // redirect to login page if not logged in
+        if (next.authenticate && !$rootScope.currentUser) {
+          event.preventDefault(); //prevent current page from loading
+          $state.go('forbidden');
+        }
+      });
     }]);
 })();
